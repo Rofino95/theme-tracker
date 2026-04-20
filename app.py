da070109b41a2406acabb94b0dc50037869d2b7d
RobinZ <rofinosgt@gmail.com>
2026-04-20 15:17:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -163,7 +163,7 @@ st.dataframe(
     }),
     use_container_width=True,
     hide_index=True
-    height=height
+    height=800
 )
 
 st.info(
@@ -275,5 +275,5 @@ st.dataframe(
     }),
     use_container_width=True,
     hide_index=True
-    height=height
+    height=800
 )
